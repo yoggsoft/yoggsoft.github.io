144bc706d467b9ecf074e9e78fe6e99f12470dd6
Manuel Reyes <yoggsoft@gmail.com>
2016-05-11 06:06:25 +0000

    Added > Angular, skill controller, hardcoded skill data

---
+++ prod/js/logic.js
@@ -0,0 +1,10 @@
+(function(){
+    var setupDom = function () {
+        var dom = {};
+        console.log(dom);
+        dom.overlay = document.getElementById("overlay");
+        console.log(dom.overlay);
+    }
+})(window);
+            
+            
---
+++ prod/js/resume.js
@@ -0,0 +1,36 @@
+/*global angular*/
+'use strict';
+var resume = angular.module('resume',[]);
+
+resume.controller('rightController', ['$scope', function($scope) {
+    this.skills = {
+        techskills: {
+            label:"Technical Skills",
+            data:[
+                'HTML5',
+                'CSS3 - SASS',
+                'AngularJS',
+                'Git',
+                'ActionScript',
+                'Javascript',
+                'Node - Grunt',
+                'jQuery',
+                'Rich Media',
+                'PHP'
+                ]
+        },
+        certs: {
+            label:"Certification & Awards",
+            data:['DoubleClick','Autodesk Maya','Google Web Designer']
+        },
+        languages: {
+            label:"Languages",
+            data:['Spanish','English','Portuguese','German','French']
+        },
+        interests: {
+            label:"Interests",
+            data:['Internet','Music','Videogames','Real Madrid','Technology','Development']
+        }
+    };
+}]);
+
