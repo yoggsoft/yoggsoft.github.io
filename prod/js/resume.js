38e89b48b0a830a2b3e2d37eb49cc7807c18fc20
Manuel Reyes <yoggsoft@gmail.com>
2016-05-12 01:59:06 +0000

    Added > fluid image

--- prod/js/resume.js
+++ prod/js/resume.js
@@ -12,19 +12,28 @@ resume.controller('mainController',['$scope',function($scope){
 resume.controller('leftController',['$scope',function($scope){
     this.icons = {
         gmail:{
+            label:"gmail",
             icon:"fa fa-envelope-o fa-2x",
             path:"mailto:yoggsoft@gmail.com"
         },
         github:{
+            label:"github",
             icon:"fa fa-github fa-2x",
             path:"https://www.github.com/yoggsoft"
         },
+        
         linkedin:{
+            label:"linkedin",
             icon:"fa fa-linkedin fa-2x",
             path:"https://ar.linkedin.com/in/querales"
+        },
+        codepen:{
+            label:"codepen",
+            icon:"fa fa-codepen fa-2x",
+            path:"https://codepen.io/yoggsoft/"
         }
         
-    }
+    };
 }]);
 
 resume.controller('rightController', ['$scope', function($scope) {
